73cb0f5289a6bb27d7ae1a6149e10b7d02ba6dcf
Eric Ford <ebf11@psu.edu>
2021-03-24 03:29:20 -0400

    trying wo conda

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,12 @@ using Pkg
 Pkg.build("PyCall")
 
 using Conda
+pipcmd = joinpath(Conda.PYTHONDIR,"pip")
+run(`$pipcmd install pyneid`)
+
+#=
 env = Conda.ROOTENV
 Conda.pip_interop(true, env)
 Conda.pip("install", "pyneid")
+=#
+
